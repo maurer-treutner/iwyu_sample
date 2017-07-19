26d819ccf9613f3d17469374531edb00f1b175ac
Peter Maurer <p.maurer@maurer-treutner.de>
2017-07-19 17:14:08 +0200

    Installation to /usr/local instead of /usr Added make.log

--- doc/installation.sh
+++ doc/installation.sh
@@ -1,9 +1,8 @@
-cd /tmp
 curl -O https://include-what-you-use.org/downloads/include-what-you-use-0.7-x86_64-linux-gnu-ubuntu-16.04.tar.gz
 tar -xvzf include-what-you-use-0.7-x86_64-linux-gnu-ubuntu-16.04.tar.gz
-sudo mv include-what-you-use/bin/* /usr/bin
-sudo mv include-what-you-use/lib/* /usr/lib/
-sudo mkdir /usr/share/clang/include-what-you-use/
-sudo mv include-what-you-use/mappings /usr/share/clang/include-what-you-use/
-sudo ln /usr/bin/include-what-you-use /usr/bin/iwyu
+sudo mv include-what-you-use/bin/* /usr/local/bin
+sudo mv include-what-you-use/lib/* /usr/local/lib/
+sudo mkdir -p /usr/local/share/clang/include-what-you-use/
+sudo mv include-what-you-use/mappings /usr/local/share/clang/include-what-you-use/
+sudo ln /usr/local/bin/include-what-you-use /usr/local/bin/iwyu
 
